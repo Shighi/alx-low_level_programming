d440e08cd7ad8a849ca7fa9025c7bb3607fe8d40
Shighi <daisyshighi@gmail.com>
2024-06-22 11:04:19 -0700

    Betty linter

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
-int main(void) { puts("Programming is like building a multilingual puzzle"); return (0); }
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints "Programming is like building
+ * a multilingual puzzle" followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
