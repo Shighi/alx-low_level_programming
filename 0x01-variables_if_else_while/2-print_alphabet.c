b04b8db2fd666d255bc115d4524a4d44ff11e1ac
Shighi <daisyshighi@gmail.com>
2024-06-27 07:32:24 -0700

    betty dec alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet
+ *
+ * Description: This program prints all lowercase letters
+ * of the alphabet from 'a' to 'z', followed by a newline.
+ * It uses a while loop and the putchar function.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 char letter = 'a';
