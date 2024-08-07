b4aa36de6df06d8e5447f1be500d62b8763cda22
Shighi <daisyshighi@gmail.com>
2024-08-06 23:43:41 -0700

    review

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,43 +1,44 @@
+#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "lists.h"
 
 /**
- * print_listint_safe - prints a listint_t linked list safely
- * @head: pointer to the head of the list
- * Return: the number of nodes in the list
+ * print_listint_safe - Prints a listint_t linked list safely.
+ * @head: Pointer to the head of the list.
+ *
+ * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *slow = head, *fast = head;
+	const listint_t *current = head;
 	size_t count = 0;
+	const listint_t *addresses[1024]; /* Assuming max 1024 nodes */
 
-	while (slow && fast && fast->next)
+	while (current)
 	{
-		printf("[%p] %d\n", (void *)slow, slow->n);
-		count++;
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
+		size_t i;
+
+		for (i = 0; i < count; i++)
 		{
-			printf("[%p] %d\n", (void *)slow, slow->n);
-			count++;
-			slow = slow->next;
-			while (slow != fast)
+			if (current == addresses[i])
 			{
-				printf("[%p] %d\n", (void *)slow, slow->n);
-				count++;
-				slow = slow->next;
+				printf("-> [%p] %d\n", (void *)current, current->n);
+				return (count);
 			}
-			printf("-> [%p] %d\n", (void *)slow, slow->n);
-			return (count);
 		}
-	}
-	while (head)
-	{
-		printf("[%p] %d\n", (void *)head, head->n);
+
+		printf("[%p] %d\n", (void *)current, current->n);
+		addresses[count] = current;
 		count++;
-		head = head->next;
+
+		if (count >= 1024)
+		{
+			fprintf(stderr, "Error: List too long\n");
+			exit(98);
+		}
+
+		current = current->next;
 	}
+
 	return (count);
 }
--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,40 +1,49 @@
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * free_listint_safe - frees a listint_t list safely
- * @h: pointer to pointer to the head of the list
- * Return: the size of the list that was freed
+ * free_listint_safe - Frees a listint_t list safely.
+ * @h: Pointer to the pointer of the head of the list.
+ *
+ * Return: The size of the list that was freed.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *slow, *fast, *temp;
+	listint_t *current, *temp;
 	size_t count = 0;
+	listint_t *addresses[1024]; /* Assuming max 1024 nodes */
 
 	if (!h || !*h)
 		return (0);
 
-	slow = fast = *h;
-	while (slow && fast && fast->next)
+	current = *h;
+	while (current)
 	{
-		temp = slow;
-		slow = slow->next;
-		fast = fast->next->next;
+		size_t i;
+
+		for (i = 0; i < count; i++)
+		{
+			if (current == addresses[i])
+			{
+				*h = NULL;
+				return (count);
+			}
+		}
+
+		addresses[count] = current;
 		count++;
-		free(temp);
-		if (slow == fast)
+
+		if (count >= 1024)
 		{
 			*h = NULL;
 			return (count);
 		}
-	}
-	while (slow)
-	{
-		temp = slow;
-		slow = slow->next;
-		count++;
+
+		temp = current;
+		current = current->next;
 		free(temp);
 	}
+
 	*h = NULL;
 	return (count);
 }
