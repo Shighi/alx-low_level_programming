664a70c763a6bb3918a264062196ac32ab2fca82
Shighi <daisyshighi@gmail.com>
2024-09-23 12:55:25 -0700

    to t3

---
+++ 0x18-dynamic_libraries/iwin.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+int rand(void)
+{
+    static int ct = -1;
+
+    ct++;
+    switch (ct) {
+        case 0: return 9;
+        case 1: return 8;
+        case 2: return 10;
+        case 3: return 24;
+        case 4: return 75;
+        case 5: return 9;
+        default: return rand();
+    }
+}
